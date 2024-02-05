bd40013da48530ea647a957a7bd04b4bf822ccd1
Himanshu <pandeyrocks1210@gmail.com>
2024-02-05 19:56:32 +0530

    Project complete

--- src/Components/News.js
+++ src/Components/News.js
@@ -10,32 +10,33 @@ export default function News(props) {
   const [loader, setLoader] = useState(false);
   const [total, setTotal] = useState(0);
 
-  useEffect(() => {
+
+  useEffect(() =>{
+    document.title = `${capitalizeFirstLetter(props.category)} - News App`;
     props.setProgress(10);
-    const fetchData= async () =>{
-      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api}&page=${page}&pageSize=${props.pageSize}`;
-
-      setLoader(true);
-      props.setProgress(25);
-      let data = await fetch(url);
-      let parsedData = await data.json();
-
-      props.setProgress(75);
-
-      setTotal(parsedData.totalResults);
-      setArr((prev) =>{
-        const newArr=prev.concat(parsedData.articles);
-        return newArr;
-      })
-      setLoader(false);
-      
-      props.setProgress(100);
-    }
-
     fetchData();
-  }, [props.country, props.category, page, props.pageSize]);
+  }, [])
 
+  const fetchData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api}&page=${page}&pageSize=${props.pageSize}`;
 
+    setLoader(true);
+    props.setProgress(25);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    props.setProgress(75);
+
+    setTotal(parsedData.totalResults);
+    setArr(parsedData.articles);
+    setLoader(false);
+
+    props.setProgress(100);
+  }
+
+  function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   // function getPrev(){
   //   setPage((prev) =>{
   //     return Math.max(prev-1, 1);
@@ -48,40 +49,45 @@ export default function News(props) {
   //   })
   // }
 
-  function fetchMore(){
-    setPage((prev) =>{
-      return prev+1;
+  const fetchMore = async () => {   
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api}&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page+1) 
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    setArr((prev) =>{
+      return prev.concat(parsedData.articles);
     })
-  }
+  };
 
   return (
     <div className='my-2 text-center'>
-      <h1 className='text-center' style={{ margin: '20px 0px' }}>NewsApp - Top Headlines</h1>
+      <h1 className='text-center' style={{ margin: '20px 0px' }}>NewsApp - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
       {loader && <Spinner />}
       <InfiniteScroll
         dataLength={arr.length}
         next={fetchMore}
-        hasMore={arr.length!==total}
+        hasMore={arr.length !== total}
         loader={<Spinner />}
       >
 
-      
-          <div className="row">
-            {arr.map((element) => {
-              return <div className="col-md-4">
-                <NewsItems 
-                title={element.title} 
-                description={element.description} 
-                imageUrl={element.urlToImage} 
-                newsUrl={element.url} 
-                author={element.author} 
-                date={element.publishedAt} 
-                name={element.source.name} 
-                />
-                </div>
+
+        <div className="row">
+          {arr.map((element) => {
+            return <div className="col-md-4">
+              <NewsItems 
+                key={element.url}
+                title={element.title}
+                description={element.description}
+                imageUrl={element.urlToImage}
+                newsUrl={element.url}
+                author={element.author}
+                date={element.publishedAt}
+                name={element.source.name}
+              />
+            </div>
           })}
-          </div>
-        </InfiniteScroll>
+        </div>
+      </InfiniteScroll>
 
       {/* <div class="d-flex justify-content-between my-2">
         <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={getPrev}>&larr;Previous</button>
@@ -92,6 +98,5 @@ export default function News(props) {
 
   )
 }
-
 
 
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NewsItems(props) {
   return (
   <div className="card">
     <img src={props.imageUrl?props.imageUrl:"https://www.hollywoodreporter.com/wp-content/uploads/2023/07/Haunted-Mansion-Barbie-and-Oppenheimer-Split-H-2023.jpg?w=1024"} className="card-img-top" alt="..."/>
-    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:1}}>
+    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:1}}>
       {props.name}
-      <span class="visually-hidden">unread messages</span>
+      <span className="visually-hidden">unread messages</span>
     </span>
     <div className="card-body">
       <h5 className="card-title">{props.title}</h5>
       <p className="card-text">{props.description}</p>
-      <p class="card-text"><small class="text-body-secondary">By {props.author?props.author:"Unknown"} on {props.date}</small></p>
-      <a href={props.newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+      <p className="card-text"><small className="text-body-secondary">By {props.author?props.author:"Unknown"} on {props.date}</small></p>
+      <Link to={props.newsUrl} target="_blank" className="btn btn-primary">Read More</Link>
     </div>
   </div>
     
